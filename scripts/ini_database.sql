be27530d4806840953ea2fa699c2dce5ecd6c92e
Isaque Ferreira <97652099+CFIsaque@users.noreply.github.com>
2026-01-20 18:03:34 -0300

    Initialize database schemas for bronze, prata, and ouro

---
+++ scripts/ini_database.sql
@@ -0,0 +1,17 @@
+--Bronze
+
+CREATE SCHEMA IF NOT EXISTS bronze
+OPTIONS (
+  location = "US"
+);
+
+--Prata
+CREATE SCHEMA IF NOT EXISTS prata
+OPTIONS (
+  location = "US"
+);
+
+CREATE SCHEMA IF NOT EXISTS ouro
+OPTIONS (
+  location = "US"
+);
