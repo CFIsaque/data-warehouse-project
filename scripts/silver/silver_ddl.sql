490fac5b16e7d8fb25e5e821282f42f68a358ae2
Isaque Ferreira <97652099+CFIsaque@users.noreply.github.com>
2026-02-03 17:31:31 -0300

    Modify column types in silver DDL script
    Updated data types for several columns in silver tables.

--- scripts/silver/silver_ddl.sql
+++ scripts/silver/silver_ddl.sql
@@ -1,3 +1,13 @@
+/*
+-----------------------------------------------------------
+
+DDL Script: Create Silver Tables
+
+  This Script creates tables in the 'silver' schema, dropping existing table
+  if they already exist
+  Run this script to re-define the DDL sctructure of 'silver' Tables
+-----------------------------------------------------------
+*/
 CREATE OR REPLACE TABLE silver.crm_customer_info(
   cst_id INT64,
   cst_key STRING,
@@ -11,6 +21,7 @@ CREATE OR REPLACE TABLE silver.crm_customer_info(
 
 CREATE OR REPLACE TABLE silver.crm_prd_info(
   prd_id       INT64,
+  cat_id       STRING,
   prd_key      STRING,
   prd_nm       STRING,
   prd_cost     INT64,
@@ -24,9 +35,9 @@ CREATE OR REPLACE TABLE silver.crm_sales_details(
   sls_ord_num  STRING,
   sls_prd_key  STRING,
   sls_cust_id  INT64,
-  sls_order_dt INT64,
-  sls_ship_dt  INT64,
-  sls_due_dt   INT64,
+  sls_order_dt DATE,
+  sls_ship_dt  DATE,
+  sls_due_dt   DATE,
   sls_sales    INT64,
   sls_quantity INT64,
   sls_price    INT64,
@@ -45,6 +56,6 @@ CREATE OR REPLACE TABLE silver.erp_px_cat_g1v2 (
   id STRING,
   cat STRING,
   subcat STRING,
-  maintenance BOOL,
+  maintenance STRING,
   ingestion_timestamp TIMESTAMP  
 );
