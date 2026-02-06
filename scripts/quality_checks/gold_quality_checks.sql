21800f837eb58d0be92030ad75b74a769c462cad
Isaque Ferreira <97652099+CFIsaque@users.noreply.github.com>
2026-02-06 15:12:46 -0300

    Fix SQL quality checks for gold layer integrity
    Updated SQL script for gold quality checks to correct comments and ensure proper checks for uniqueness of product and customer keys.

--- scripts/quality_checks/gold_quality_checks.sql
+++ scripts/quality_checks/gold_quality_checks.sql
@@ -1,20 +1,41 @@
 /*
 Quality checks for gold
+
+Script Purpose:
+  This script performs quality checks to validate the integrity, consistency,
+  and accuracy of the Gold Layer. Theses checks ensure:
+  - Uniqueness of surrogate keys in the dimension tables.
+  - Uniqueness of surrogate keys in the dimension tables.
+  - Referential integrity between fact and dimension tables.
+  - Validation of relationships in the data model for analytical purposes.
 */
 
-SELECT product_id, count(*)
+-- ========================================================================
+-- Checking 'gold.dim_products'
+-- ========================================================================
+-- Check for Uniqueness of Product Key in gold.dim_customers
+-- Expectation: No results
+
+SELECT product_key, count(*)
 FROM gold.dim_products
 GROUP BY product_id
 HAVING count(*) > 1;
 
+-- ========================================================================
+-- Checking 'gold.dim_customers'
+-- ========================================================================
+-- Check for Uniqueness of Customers Key in gold.dim_customers
+-- Expectation: No results
 
-
-SELECT customer_id, count(*)
+SELECT customer_key, count(*)
 FROM gold.dim_customers
 GROUP BY customer_id
 HAVING count(*) > 1;
 
-
+-- ========================================================================
+-- Checking 'gold.fact_sales'
+-- ========================================================================
+-- Check the data model connectivity between fact and dimensions
 
 SELECT * FROM `integration-projeto.gold.fact_sales` s
 LEFT JOIN  gold.dim_products p
